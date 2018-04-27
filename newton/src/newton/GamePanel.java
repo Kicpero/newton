221a4f376f6ff3b1c8d9be223a0ee246fa91ab6e
ola <ola@ola-okrutny>
2018-04-27 16:03:07 +0200

    First commit

---
+++ newton/src/newton/GamePanel.java
@@ -0,0 +1,117 @@
+package newton;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import newton.MainPanel;
+
+public class GamePanel extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+	public GamePanel(MainPanel main)  {
+		
+		mPanel=main;
+
+		// right Panel
+		rightPanel= new JPanel();
+		time=new JLabel("CZAS:");
+		time_to_end= new JLabel("...");
+		fuel=new JLabel("PALIWO");
+		level_fuel= new JLabel ("...");
+		points=new JLabel("PUNKTY:");
+		score_points= new JLabel("...");
+		exit=new JButton("Powrót");
+		exit.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+			}
+		});
+		
+		if(mPanel.n=="pl")
+		{
+			exit.setText("Powrót");
+			time.setText("Czas");
+			points.setText("Punkty");
+			fuel.setText("Paliwo");
+			}
+		if(mPanel.n=="en")
+		{
+			exit.setText("Back");
+			time.setText("Time");
+			points.setText("Points");
+			fuel.setText("Fuel");
+		}
+		
+		rightPanel.setLayout(new FlowLayout());
+		rightPanel.add(fuel);
+		rightPanel.add(level_fuel);
+		rightPanel.add(time);
+		rightPanel.add(time_to_end);
+		rightPanel.add(points);
+		rightPanel.add(score_points);
+		rightPanel.add(exit);
+		rightPanel.setVisible(true);
+		rightPanel.setPreferredSize(new Dimension(500, 30));
+		rightPanel.setBackground(Color.WHITE);
+		
+		// koniec right Panel
+		
+		// center Panel
+		
+		centerPanel = new JPanel();
+		centerPanel.setSize(500, 500);
+		java.net.URL im = getClass().getResource("background2.jpg");//path to image
+		ImageIcon imageIcon = new ImageIcon(im); // load the image to a imageIcon
+		Image image = imageIcon.getImage(); // transform it 
+		Image newimg = image.getScaledInstance(centerPanel.getWidth(), centerPanel.getHeight(),java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
+		imageIcon = new ImageIcon(newimg);  // transform it back
+		centerPanel.add(new JLabel(imageIcon));
+		
+		
+		// koniec center Panel
+		
+		//razem
+		this.setSize(500, 550);
+		this.setLayout( new BorderLayout());
+		this.add(rightPanel, BorderLayout.NORTH);
+		this.add(centerPanel, BorderLayout.CENTER);
+		
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+		
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		
+	}
+	
+	/*public void repaint(Graphics g) {
+	}*/
+	
+	MainPanel mPanel;
+	JPanel rightPanel;
+	JPanel centerPanel;
+	JLabel time;
+	JLabel points;
+	JLabel fuel;
+	JLabel level_fuel;
+	JLabel time_to_end;
+	JLabel score_points;
+	JButton exit;
+
+}
---
+++ newton/src/newton/OptionPanel.java
@@ -0,0 +1,89 @@
+package newton;
+
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class OptionPanel  extends JFrame{
+	private static final long serialVersionUID = 1L;
+
+	
+	
+	public OptionPanel(MainPanel main) {
+		
+		mainPanel=main;
+		
+		vehicle_color = new JButton("Wybierz kolor rakiety");
+		
+		vehicle_color.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				color = JColorChooser.showDialog(null,"Choose Color",color);
+				
+				
+			}
+		});
+		exit=new JButton("Powrót");
+		exit.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+			}
+		});
+		if(mainPanel.n=="pl")
+		{
+			exit.setText("Powrót");
+			vehicle_color.setText("Wybierz kolor rakiety");
+		}
+		if(mainPanel.n=="en")
+		{
+			exit.setText("Back");
+			vehicle_color.setText("Choose color of vehicle");
+		}
+        this.setSize(300, 300);
+        
+        java.net.URL im = getClass().getResource("background.jpg"); // path to image
+        ImageIcon imageIcon = new ImageIcon(im); // load the image to a imageIcon
+		Image image = imageIcon.getImage(); // transform it 
+		Image newimg = image.getScaledInstance(this.getWidth(), this.getHeight(),java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
+		imageIcon = new ImageIcon(newimg);  // transform it back
+		setContentPane(new JLabel(imageIcon));
+		this.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		rankingPanel=new JPanel();
+		
+		rankingPanel.setLayout(new GridLayout(2,1));
+		rankingPanel.add(vehicle_color);
+		
+		rankingPanel.add(exit);
+		
+		c.ipady = 40;   
+		c.ipadx = 60;
+		c.gridwidth = 0;
+		c.gridx = 1;
+		c.gridy = 1;
+		this.add(rankingPanel, c);
+        
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+	}
+
+	
+
+	JPanel rankingPanel;
+	JButton vehicle_color;
+	JButton exit;
+	Color color;
+	MainPanel mainPanel;
+	
+
+}
