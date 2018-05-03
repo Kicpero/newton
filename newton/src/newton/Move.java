55ce1366464aa0184b538958d756e3c8155b847e
ola <ola@ola-okrutny>
2018-05-03 14:25:02 +0200

    14:25

--- newton/src/newton/Move.java
+++ newton/src/newton/Move.java
@@ -17,6 +17,7 @@ public class Move {
 		scheduler.scheduleAtFixedRate((new Runnable() {
 			public void run() {
 				
+				
 				sym=new Simulation(draw.objects);
 				sym.run();
 				for (int i = 0; i<draw.objects.size();i++) {
